f67bc5859654f68d967c6c82085af0d148916825
jacobjshelp <jacobjshelp>
2024-09-20 09:46:04 +0400

    Step 2 - Basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import './styles/App.css'
+
 function App() {
-  return <div>App</div>
+  return (
+    <div id="container">
+      <div id="header">header</div>
+      <div id="mainSection">mainSection</div>
+      <div id="footer">footer</div>
+    </div>
+  )
 }
 
 export default App
